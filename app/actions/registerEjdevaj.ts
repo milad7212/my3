cb0f7ea98c868ab2d2bdb6f8397cda9a10803c86
Milad Hasani <milad7212@gmail.com>
2024-06-26 08:43:33 +0330

    change

--- app/actions/registerEjdevaj.ts
+++ app/actions/registerEjdevaj.ts
@@ -1,156 +1,179 @@
 "use server";
 
-import puppeteer from "puppeteer";
+import axios from "axios";
+import puppeteer, { Page } from "puppeteer";
+interface Data {
+  codeMeli: string;
+  dayTavalod: string;
+  monthTavalod: string;
+  yearTavalod: string;
+  dayEjdevag: string;
+  monthEjdevag: string;
+  yearEjdevag: string;
+  phoneNumber: string;
+  phoneStatic: string;
+  zipCode: string;
+  address: string;
+  ostan: string;
+  city: string;
+}
 
-const ostan = [
-  { value: 0, text: "-- انتخاب کنيد --" },
-  { value: 4, text: "آذربايجان شرقي" },
-  { value: 5, text: "آذربايجان غربي" },
-  { value: 25, text: "اردبيل" },
-  { value: 11, text: "اصفهان" },
-  { value: 32, text: "البرز" },
-  { value: 23, text: "ايلام" },
-  { value: 16, text: "بوشهر" },
-  { value: 24, text: "تهران" },
-  { value: 20, text: "چهارمحال و بختياري" },
-  { value: 31, text: "خراسان جنوبي" },
-  { value: 30, text: "خراسان رضوي" },
-  { value: 29, text: "خراسان شمالي" },
-  { value: 7, text: "خوزستان" },
-  { value: 19, text: "زنجان" },
-  { value: 21, text: "سمنان" },
-  { value: 12, text: "سيستان و بلوچستان" },
-  { value: 8, text: "فارس" },
-  { value: 28, text: "قزوين" },
-  { value: 26, text: "قم" },
-  { value: 13, text: "كردستان" },
-  { value: 9, text: "كرمان" },
-  { value: 6, text: "كرمانشاه" },
-  { value: 22, text: "كهكيلويه و بويراحمد" },
-  { value: 27, text: "گلستان" },
-  { value: 2, text: "گيلان" },
-  { value: 15, text: "لرستان" },
-  { value: 3, text: "مازندران" },
-  { value: 1, text: "مركزي" },
-  { value: 17, text: "هرمزگان" },
-  { value: 14, text: "همدان" },
-  { value: 18, text: "يزد" },
-];
-const kermanCity = [
-  { value: 0, text: "-- انتخاب کنید --" },
-  { value: 816, text: "ارزوییه" },
-  { value: 475, text: "انار" },
-  { value: 803, text: "بافت" },
-  { value: 805, text: "بردسیر" },
-  { value: 804, text: "بم" },
-  { value: 801, text: "جیرفت" },
-  { value: 817, text: "رابر" },
-  { value: 810, text: "راور" },
-  { value: 444, text: "رفسنجان" },
-  { value: 813, text: "رودبارجنوب" },
-  { value: 917, text: "ریگان" },
-  { value: 806, text: "زرند" },
-  { value: 802, text: "سیرجان" },
-  { value: 807, text: "شهربابک" },
-  { value: 812, text: "عنبرآباد" },
-  { value: 820, text: "فاریاب" },
-  { value: 818, text: "فهرج" },
-  { value: 814, text: "قلعه گنج" },
-  { value: 809, text: "محمدآباد" },
-  { value: 815, text: "منوجان" },
-  { value: 819, text: "نرماشیر" },
-  { value: 800, text: "کرمان" },
-  { value: 808, text: "کهنوج" },
-  { value: 811, text: "کوهبنان" },
-];
-// name:
-// codeMeli:
-// dayTavalod:
-// monthTavalod:
-// yearTavalod: ,
-// dayEjdevag:
-// monthEjdevag:
-// yearEjdevag:
-// phoneNumber:
-// ostan:
+export async function registerEjdevag(data: Data): Promise<void> {
+  const width = 1920; // عرض صفحه نمایش
+  const height = 1080;
 
-export async function registerEjdevag(data) {
-  try {
-    (async () => {
-      // Launch the browser and open a new blank page
-      const browser = await puppeteer.launch({ headless: false });
-      const page = await browser.newPage();
+  // تابعی برای گرفتن اسکرین‌شات
+  async function takeScreenshot(page: Page) {
+    // اضافه کردن دیالوگ شبیه‌سازی شده به صفحه
 
-      // Navigate the page to a URL
-      await page.goto("https://ve.cbi.ir/TasRequest.aspx");
-
-      // Set screen size
-      //   await page.setViewport({ width: 1440, height: 1024 });
-      const firstInput = await page.waitForSelector(
-        "#ctl00_ContentPlaceHolder1_tbIDNo"
-      );
-
-      await page.type("#ctl00_ContentPlaceHolder1_tbIDNo", data.codeMeli);
-
-      await page.type("#ctl00_ContentPlaceHolder1_ddlBrDay", data.dayTavalod);
-      await page.select(
-        "#ctl00_ContentPlaceHolder1_ddlBrMonth",
-        data.monthTavalod
-      );
-      await page.type("#ctl00_ContentPlaceHolder1_tbBrYear", data.yearTavalod);
-
-      // تاریخ ازدواج
-      // روز
-      await page.select(
-        "#ctl00_ContentPlaceHolder1_ddlMarryDay",
-        data.dayEjdevag
-      );
-      // ماه
-      await page.select(
-        "#ctl00_ContentPlaceHolder1_ddlMarryMonth",
-        data.monthEjdevag
-      );
-      // سال
-      await page.type(
-        "#ctl00_ContentPlaceHolder1_tbMarrYear",
-        data.yearEjdevag
-      );
-
-      // شماره موبایل
-      await page.type(
-        "#ctl00_ContentPlaceHolder1_tbMobileNo",
-        data.phoneNumber
-      );
-
-      // استان
-      await page.select("#ctl00_ContentPlaceHolder1_ddlState", data.ostan);
-
-      // صفحه دومم
-
-      
-      // انتخاب شهر
-
-      await page.select("#ctl00_ContentPlaceHolder1_ddlCity", data.city);
-
-      // شماره تلفن ثابت
-      const firstInputSecoundPage = await page.waitForSelector(
-        "#ctl00_ContentPlaceHolder1_tbTel"
-      );
-
-      // شماره تلفن ثابت
-
-      await page.type("#ctl00_ContentPlaceHolder1_tbTel", data.phoneStatic);
-
-      // کد پستی
-      await page.type("#ctl00_ContentPlaceHolder1_tbZipCD", data.zipCode);
-
-      // نشانی محل سکونت
-      await page.type("#ctl00_ContentPlaceHolder1_tbAddress", data.address);
-
-      //   await browser.close();
-    })();
-  } catch (error) {
-    console.log("error", error);
-    return null;
+    const date = new Date();
+    const dateString = `${date.getFullYear()}-${(date.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+    const timeString = `${date.getHours().toString().padStart(2, "0")}-${date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}-${date.getSeconds().toString().padStart(2, "0")}`;
+    const fileName = `screenshot-${dateString}-${timeString}.png`;
+    await page.screenshot({ path: fileName });
+    // حذف دیالوگ شبیه‌سازی شده از صفحه
+    await page.evaluate(() => {
+      let dialogDiv = document.getElementById("custom-dialog");
+      if (dialogDiv) {
+        dialogDiv.remove();
+      }
+    });
   }
+
+  // تابعی برای تلاش به بارگذاری صفحه تا 3 بار
+  async function tryLoadPage(page, url) {
+    for (let attempt = 1; attempt <= 3; attempt++) {
+      try {
+        await page.goto(url, {
+          waitUntil: "networkidle2",
+          timeout: 30000,
+        });
+        return true; // بارگذاری موفقیت‌آمیز
+      } catch (error) {
+        // console.log(`Attempt ${attempt} failed: ${error.message}`);
+        if (attempt === 3) {
+          return false; // بعد از سه بار تلاش شکست خورد
+        }
+      }
+    }
+  }
+
+  // تابع اصلی برای ثبت ازدواج
+  async function register() {
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: [`--window-size=${width},${height}`],
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: width, height: height });
+    // جایگزینی تابع alert
+    await page.evaluate(() => {
+      window.alert = (message) => {
+        let event = new CustomEvent("alert", { detail: message });
+        window.dispatchEvent(event);
+      };
+    });
+
+    // مدیریت دیالوگ‌ها و گرفتن اسکرین‌شات
+    page.on("dialog", async (dialog) => {
+      // console.log("متن دیالوگ", dialog.message());
+      // console.log("milaaaad", dialog.message().includes("چند"));
+      await takeScreenshot(page, dialog.message());
+      if (dialog.message().includes("چند")) {
+        await dialog.accept();
+        setTimeout(async () => {
+          await dialog.accept();
+          await page.click("#ctl00_ContentPlaceHolder1_btnSubmit"); // کلیک دوباره روی دکمه "اوکی"
+        }, 60000);
+      } else {
+        await dialog.accept();
+      }
+    });
+
+    const pageLoaded = await tryLoadPage(
+      page,
+      "https://ve.cbi.ir/TasRequest.aspx"
+    );
+    if (!pageLoaded) {
+      // console.log("Failed to load page after 3 attempts, closing browser");
+      await browser.close();
+      return;
+    }
+
+    // پر کردن فرم
+    const firstInput = await page.waitForSelector(
+      "#ctl00_ContentPlaceHolder1_tbIDNo"
+    );
+    await page.type("#ctl00_ContentPlaceHolder1_tbIDNo", data.codeMeli);
+    await page.type("#ctl00_ContentPlaceHolder1_ddlBrDay", data.dayTavalod);
+    await page.select(
+      "#ctl00_ContentPlaceHolder1_ddlBrMonth",
+      data.monthTavalod
+    );
+    await page.type("#ctl00_ContentPlaceHolder1_tbBrYear", data.yearTavalod);
+
+    await page.select(
+      "#ctl00_ContentPlaceHolder1_ddlMarryDay",
+      data.dayEjdevag
+    );
+    await page.select(
+      "#ctl00_ContentPlaceHolder1_ddlMarryMonth",
+      data.monthEjdevag
+    );
+    await page.type("#ctl00_ContentPlaceHolder1_tbMarrYear", data.yearEjdevag);
+
+    await page.type("#ctl00_ContentPlaceHolder1_tbMobileNo", data.phoneNumber);
+    await page.select("#ctl00_ContentPlaceHolder1_ddlState", data.ostan);
+
+    const captchaInput = await page.waitForSelector(
+      "#ctl00_ContentPlaceHolder1_tbCaptcha"
+    );
+    // ارسال کپچا به سرور
+    let captcha = await getCaptchaSrc(page);
+
+    await captchaInput?.type(`${captcha}`);
+    await captchaInput?.focus();
+
+    await page.select("#ctl00_ContentPlaceHolder1_ddlCity", data.city);
+    await page.type("#ctl00_ContentPlaceHolder1_tbTel", data.phoneStatic);
+    await page.type("#ctl00_ContentPlaceHolder1_tbZipCD", data.zipCode);
+    await page.type("#ctl00_ContentPlaceHolder1_tbAddress", data.address);
+
+    await browser.close();
+  }
+
+  // اجرای تابع ثبت نام هر 1 دقیقه (60000 میلی‌ثانیه)
+  // setInterval(register, 60000);
+
+  // اولین اجرا به محض شروع برنامه
+  await register();
+}
+
+// ارسال عکس به سرور
+async function sendCaptchaToServer(src) {
+  try {
+    const response = await axios.post(
+      "http://141.98.210.70:8000/marriage-baby",
+      {
+        src: src,
+      }
+    );
+    console.log("Server response:", response.data.result);
+    return response.data.result;
+  } catch (error) {
+    console.error("Error sending image to server:", error);
+  }
+}
+
+async function getCaptchaSrc(page) {
+  const src = await page.evaluate(() => {
+    return document?.querySelector("#ctl00_ContentPlaceHolder1_ImgCaptcha").src;
+  });
+  const response = await sendCaptchaToServer(src);
+  return response;
 }
--- app/components/ui/Item.tsx
+++ app/components/ui/Item.tsx
@@ -5,7 +5,7 @@ const Item = ({ title, text }) => {
     <>
       <div className="">
         <p className="text-xs mb-2 text-gray-400"> {title}</p>
-        <p className="font-bold text-justify"> {text}</p>
+        <p className=" text-justify"> {text}</p>
       </div>
     </>
   );
