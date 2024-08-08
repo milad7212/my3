d59210428be5bbaefbb6b126e36d730ed6603b75
Milad Hasani <milad7212@gmail.com>
2024-08-08 16:37:43 +0330

    finish

--- app/actions/fillFormPage3.js
+++ app/actions/fillFormPage3.js
@@ -1,83 +1,90 @@
 import axios from "axios";
 import { writeLog } from "./writeLog";
-
+import { solveCaptcha } from "./solveCaptcha";
 export async function fillFormPage3(page, data) {
   try {
     // Fill out the form
-    await page.select("#ctl00_ContentPlaceHolder1_ddlCity", data.city); // Rafsanjan
+    try {
+      await page.select("#ctl00_ContentPlaceHolder1_ddlCity", data?.city); // Rafsanjan
+    } catch (error) {
+      console.log("Error selecting city:", error);
+    } // Rafsanjan
     // await page.select("#ctl00_ContentPlaceHolder1_ddlBankName", "56"); // Bank Saman
 
-    await page.type("#ctl00_ContentPlaceHolder1_tbTel", data.phoneStatic);
-
-    await page.type("#ctl00_ContentPlaceHolder1_tbZipCD", data.zipCode);
-    await page.type("#ctl00_ContentPlaceHolder1_tbAddress", data.address);
-    if (data.gender) {
-      await page.select("#ctl00_ContentPlaceHolder1_ddlCity", data.sarbazi);
+    try {
+      await page.type("#ctl00_ContentPlaceHolder1_tbTel", data?.phoneStatic);
+    } catch (error) {
+      console.log("Error typing phone:", error);
     }
 
-    if (data.hasIsar === 1) {
-      await page.click("#ctl00_ContentPlaceHolder1_rbtnIsar1");
+    
+    try {
+      await page.type("#ctl00_ContentPlaceHolder1_tbZipCD", data?.zipCode);
+    } catch (error) {
+      console.log("Error typing zip code:", error);
+    }
+    try {
+      await page.type("#ctl00_ContentPlaceHolder1_tbAddress", data?.address);
+    } catch (error) {
+      console.log("Error typing address:", error);
+    }
+    if (data?.gender) {
+      try {
+        await page.select("#ctl00_ContentPlaceHolder1_ddlCity", data?.sarbazi);
+      } catch (error) {
+        console.log("Error selecting sarbazi:", error);
+      }
+    }
+
+    if (data?.hasIsar === 1) {
+      try {
+        await page.click("#ctl00_ContentPlaceHolder1_rbtnIsar1");
+      } catch (error) {
+        console.log("Error clicking Isar1:", error);
+      }
     } else {
-      await page.click("#ctl00_ContentPlaceHolder1_rbtnIsar0");
+      try {
+        await page.click("#ctl00_ContentPlaceHolder1_rbtnIsar0");
+      } catch (error) {
+        console.log("Error clicking Isar0:", error);
+      }
+    }
+    let captchaInput;
+    try {
+       captchaInput = await page.waitForSelector(
+        "#ctl00_ContentPlaceHolder1_tbCaptcha1"
+      );
+    } catch (error) {
+      console.log("Error waiting for captcha input:", error);
+    }
+    
+    try {
+      let captcha = await solveCaptcha(
+        page,
+        "#ctl00_ContentPlaceHolder1_ImgCaptcha1"
+      );
+      await captchaInput?.type(`${captcha}`);
+    } catch (error) {
+      console.log("error", error);
     }
 
     // منتظر بمانید تا سلکت باکس قابل دسترسی باشد
     await page.waitForSelector("#ctl00_ContentPlaceHolder1_ddlBankName");
 
-    // دریافت تمامی گزینه‌های سلکت باکس
-    const options = await page.evaluate(() => {
-      const selectElement = document.querySelector(
-        "#ctl00_ContentPlaceHolder1_ddlBankName"
+    try {
+      await page.select("#ctl00_ContentPlaceHolder1_ddlBankName", data?.idbank);
+    } catch (error) {
+      console.log(
+        `Option with value ${data?.idbank} not found. No action taken.`
       );
-      const optionsArray = [];
-      for (let i = 0; i < selectElement.options.length; i++) {
-        optionsArray.push({
-          value: selectElement.options[i].value,
-          text: selectElement.options[i].text,
-        });
-      }
-      return optionsArray;
-    });
-
-    writeLog(data.phoneNumber, `${options}`);
-
-    const captchaInput = await page.waitForSelector(
-      "#ctl00_ContentPlaceHolder1_ddlSarbasiST"
-    );
-
-    let captcha = await getCaptchaSrc(page);
-    await captchaInput?.type(`${captcha}`);
+    }
 
     // // Submit the form
-    // await pKage.click("#ctl00_ContentPlaceHolder1_btnSave");
+    // await page.click("#ctl00_ContentPlaceHolder1_btnSave");
 
     // await page.click("#ctl00_ContentPlaceHolder1_rbtnIsar0");
   } catch (error) {
     writeLog(data.phoneNumber, error);
     return;
   }
-}
-
-async function sendCaptchaToServer(src) {
-  try {
-    const response = await axios.post(
-      "http://146.19.212.232:8000/marriage-baby",
-      {
-        src: src,
-      }
-    );
-
-    return response.data.result;
-  } catch (error) {
-    // console.log("error captcha", error);
-    return "";
-  }
-}
-
-async function getCaptchaSrc(page) {
-  const src = await page.evaluate(() => {
-    return document?.querySelector("#ctl00_ContentPlaceHolder1_ImgCaptcha1")
-      .src;
-  });
-  return await sendCaptchaToServer(src);
 }
